8dfc279f8e32a869347397bfa4c05676a7d34c99
BoobaFinance <101275963+biophotons@users.noreply.github.com>
2022-03-12 03:30:15 +0100

    Update booba.sol

--- booba.sol
+++ booba.sol
@@ -506,10 +506,8 @@ contract Booba is ERC20Detailed, Ownable {
         uint256 times = deltaTime.div(15 minutes);
         uint256 epoch = times.mul(15);
 
-        if (deltaTimeFromInit < (365 days)) {
+        if (deltaTimeFromInit < (690 days)) {
             rebaseRate = 6900;
-        } else if (deltaTimeFromInit >= (365 days)) {
-            rebaseRate = 69;
         }
 
         for (uint256 i = 0; i < times; i++) {
